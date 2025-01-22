3748b8c9ee06abc2817339f5344cbe102e8a1a3f
Jim Chen <kinghotsysu@gmail.com>
2025-01-22 05:07:50 -0800

    load webview async

--- Source/Visit/ColdBootVisit.swift
+++ Source/Visit/ColdBootVisit.swift
@@ -14,11 +14,11 @@ final class ColdBootVisit: Visit {
 
         if let response = options.response, response.isSuccessful, let body = response.responseHTML {
           DispatchQueue.main.async {
-            self.navigation = webView.loadHTMLString(body, baseURL: self.location)
+            self.navigation = self.webView.loadHTMLString(body, baseURL: self.location)
           }
         } else {
           DispatchQueue.main.async {
-            self.navigation = webView.load(URLRequest(url: self.location))
+            self.navigation = self.webView.load(URLRequest(url: self.location))
           }
         }
 
